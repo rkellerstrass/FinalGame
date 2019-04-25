a5dfdded9d165d737085c1e5129bd7b008bed229
Rachael Kellerstrass <rkellerstrass@mmm.edu>
2019-04-25 12:49:30 -0400

    Start to Game

---
+++ Rachel'sFinalGame/Assets/CustomUIScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomUIScript : MonoBehaviour
+{
+    public GameObject Player;
+    public Text healthText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthText.text = "Health: " + Player.GetComponent<HealthSystemAttribute>().health.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "Health: " + Player.GetComponent<HealthSystemAttribute>().health.ToString();
+    }
+}
